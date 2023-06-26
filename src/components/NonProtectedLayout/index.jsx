574ac2110b4267355010d0488d492b2da467b5c4
Asya <asya@MacBook-Air-Asya.local>
2023-06-26 17:31:08 +0400

    add left side login page

--- src/components/NonProtectedLayout/index.jsx
+++ src/components/NonProtectedLayout/index.jsx
@@ -1,9 +1,19 @@
 import { Outlet } from "react-router";
+import "./index.css";
 
 const NonProtectedLayout = () => {
   return (
     <main>
-      <Outlet />
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-1 relative">
+          <div className="blueCircle">
+            <div className="greenCircle"></div>
+          </div>
+        </div>
+        <div className="col-span-2">
+          <Outlet />
+        </div>
+      </div>
     </main>
   );
 };
