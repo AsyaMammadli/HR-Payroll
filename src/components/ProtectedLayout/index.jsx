e9cba7983d4dee0da5df07359d2e421c131648ac
Asya <asya@MacBook-Air-Asya.local>
2023-06-27 16:06:19 +0400

    add header

--- src/components/ProtectedLayout/index.jsx
+++ src/components/ProtectedLayout/index.jsx
@@ -1,6 +1,9 @@
 import { Outlet } from "react-router";
 import Star from "../../assets/Star2.svg";
 import Search from "../../assets/search-normal.svg";
+import SettingsImg from "../../assets/settingpic.svg";
+import NotificationImg from "../../assets/notification.svg";
+import ProfileImg from "../../assets/profilepic.png";
 
 import "./index.css";
 const ProtectedLayout = () => {
@@ -10,20 +13,32 @@ const ProtectedLayout = () => {
         <div className="logo">
           <img src={Star} alt="Star" />
           <img src={Star} alt="Star" />
-          <span className="font-bold">TalentCore</span>
+          <span className="font-bold pl-2">TalentCore</span>
         </div>
       </aside>
 
       <main>
         <header>
-          <div className="flex">
-            <img src={Search} alt="Search" className="-me-5" />
+          <div className="inline-block relative">
+            <img src={Search} alt="Search" className="searchIcon" />
             <input
-              className="searchInput ps-3"
+              className="searchInput"
               type="text"
               placeholder="Search for jobs, candidates and more..."
             />
           </div>
+
+          <ul className="headerLinks">
+            <li>
+              <img src={SettingsImg} alt="Settings" />
+            </li>
+            <li>
+              <img src={NotificationImg} alt="Notifications" />
+            </li>
+            <li>
+              <img src={ProfileImg} alt="Profile" />
+            </li>
+          </ul>
         </header>
 
         <div className="container">
