14b180e71e06da8a2ab50381e0b1ed4447df89eb
Asya Mammadli <mammadovasya@gmail.com>
2023-07-24 21:52:39 +0400

    change login

--- src/components/NonProtectedLayout/index.jsx
+++ src/components/NonProtectedLayout/index.jsx
@@ -4,15 +4,11 @@ import "./index.css";
 const NonProtectedLayout = () => {
   return (
     <main>
-      <div className="grid grid-cols-3 gap-4">
-        <div className="col-span-1 relative">
-          <div className="blueCircle">
-            <div className="greenCircle"></div>
-          </div>
-        </div>
-        <div className="col-span-2">
-          <Outlet />
-        </div>
+      <div className="blueCircle">
+        <div className="greenCircle"></div>
+      </div>
+      <div>
+        <Outlet />
       </div>
     </main>
   );
